a7944d54e1ec5977dd6e4fe47bf6bd642f39836f
lvr1997 <lr1997rrrrr@163.com>
2020-05-14 17:44:09 +0800

    登录页面完成，及信息的基本校验

---
+++ src/components/Login.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="login-container">
+        <div class="login-box">
+            <div class="login-header">
+                <img src="../assets/images/user.png" alt="">
+            </div>
+
+            <div class="login-form">
+                <el-form :model="loginForm" :rules="rules" ref="loginForm">
+                    <el-form-item prop="userName">
+                        <el-input
+                            prefix-icon="el-icon-user"
+                            placeholder="请输入用户名"
+                            v-model="loginForm.userName"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input type="password"
+                            prefix-icon="el-icon-key"
+                            placeholder="请输入密码"
+                            v-model="loginForm.password"></el-input>
+                    </el-form-item>
+                    <el-form-item class="form-btns">
+                        <el-button type="primary" @click="doLogin('loginForm')">登录</el-button>
+                        <el-button type="info" @click="resetForm('loginForm')">重置</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        userName: '',
+        password: ''
+      },
+      rules: {
+        userName: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 5, max: 12, message: '用户名长度在 5 到 12 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 20, message: '密码长度在 6 到 20 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    doLogin (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$message({
+            message: '登录成功！',
+            type: 'success'
+          })
+        } else {
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login-box {
+    width: 400px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%)
+}
+.login-header {
+    position: absolute;
+    top: -20%;
+    left: 35%;
+}
+.login-header img {
+    border: 1px solid rgba(221, 218, 215, 0.23);
+    border-radius: 50%;
+    width: 120px;
+}
+.login-form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 25px;
+    box-sizing: border-box
+}
+.form-btns {
+    text-align: right;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/styles/global.css'
 import './plugins/element.js'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1/8088/api/v1'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
