ee3f72308aff05a8e8a3ffdd18522e8bd6cacf58
lvr1997 <lr1997rrrrr@163.com>
2020-05-20 17:56:48 +0800

    用户列表渲染

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>
-        <h4>欢迎登录科大商铺后台管理系统</h4>
+    <div class="index-container">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-card class="index-card">
+            <h4>{{ title }}</h4>
+        </el-card>
     </div>
 </template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      title: '欢迎登录科大商铺后台管理系统'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.index-container {
+    height: 100%;
+}
+.index-card {
+    margin-top: 20px;
+    height: 100%;
+}
+</style>
